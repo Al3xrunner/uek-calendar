55f8d4c4a0d46291f1995051ffeb24c9c4e63525
Oleksandr Pospielov <93545095+Al3xrunner@users.noreply.github.com>
2024-12-04 15:27:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
-# config.py
-CALENDAR_URL = 'https://apollotocal.uek.krakow.pl/calendar/group/252661'
-LANGUAGE = 'EN'  # Set to 'EN' for English or 'PL' for Polish
+
+LANGUAGE = 'EN'  # Options: 'EN' for English, 'PL' for Polish
+
+# List of schedule links
+SCHEDULE_LINKS = [
+    {
+        'name': 'main_schedule',
+        'url': 'https://apollotocal.uek.krakow.pl/calendar/group/252661',  # Replace with your actual main schedule URL
+    },
+    {
+        'name': 'eng_class',
+        'url': 'https://apollotocal.uek.krakow.pl/calendar/group/246831',  # Replace with your actual language classes URL
+    },
+    {
+        'name': 'ger_class',
+        'url': 'https://apollotocal.uek.krakow.pl/calendar/group/230031',  # Replace with your actual language classes URL
+    }
+    # Add more schedule links as needed
+]
